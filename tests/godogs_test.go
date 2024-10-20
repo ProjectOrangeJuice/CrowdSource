bf290feeb0aabea613c5d6868a7234490109a49b
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:51 +0100

    get token

--- tests/godogs_test.go
+++ tests/godogs_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"strings"
 
 	"github.com/go-resty/resty"
@@ -17,11 +19,35 @@ const base = "http://192.168.122.176:8900"
 type apiFeature struct {
 	response *resty.Response
 	request  *resty.Request
+	token    string
 }
 
 func (a *apiFeature) resetResponse(interface{}) {
 	a.response = &resty.Response{}
-	a.request = resty.New().R().SetAuthToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5VWTVOek16TURSRlFURXhRa0k0TmtWRVFVSXlOa0UxTXpCRFF6WkNNVGM1TWtNMFF6Z3dOQSJ9.eyJpc3MiOiJodHRwczovL2Rldi00eDhkOXI1eS5ldS5hdXRoMC5jb20vIiwic3ViIjoiNjFMQURkYW4yTUFtakdJMEZHOEpYcFdMc2doWDNVc0NAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcHJvamVjdC5nYXRld2F5IiwiaWF0IjoxNTg0MzgwNjA5LCJleHAiOjE1ODQ0NjcwMDksImF6cCI6IjYxTEFEZGFuMk1BbWpHSTBGRzhKWHBXTHNnaFgzVXNDIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.fEPApsvBFUA_oKupv0gJBmFGfZ5yK6uwxZ_Zlh7KjbzveyQzbO_MH-6ykhF23D_vABeuaD3liUtveQWi6UVuZCHGdwtavFwgoQKhIcxr82FkbyciMRKYNbPKcPcSLos32HHTD7A9yAbbvj82WwJCZiRTHQeSSJQuJjfNvoBcR6CVDz2IDRGK8--yUKK32KyXlkUkThJVwkfZUAf9ML3ZnGJ7yrnwrRj_qzhF_B1n2yGglNj0e6H5Loy0cSwr19ghMMWxtg9UnCvHKIGsaBoXCnPzBNH_qHAA6MpTsPpld2f3ypN_ZysBqkb0SROLVzU1ntX2fDmDvi5tYWPpzXe8FA")
+	a.request = resty.New().R().SetAuthToken(a.token)
+}
+
+func getToken() string {
+	url := "https://dev-4x8d9r5y.eu.auth0.com/oauth/token"
+
+	payload := strings.NewReader("{\"client_id\":\"61LADdan2MAmjGI0FG8JXpWLsghX3UsC\",\"client_secret\":\"urdA8PNFNeevVrvJypQFQrCvgQl72KMC2SjMyH4dvEaKOIsMuH1jsT8KesL7oczT\",\"audience\":\"https://project.gateway\",\"grant_type\":\"client_credentials\"}")
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("content-type", "application/json")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	var t map[string]interface{}
+	err := json.Unmarshal(body, &t)
+	if err != nil {
+		log.Printf("Error.. %s", err.Error())
+	}
+	token := fmt.Sprintf("%s", t["access_token"])
+	log.Printf("Token %s", t["access_token"])
+	return token
 }
 
 func (a *apiFeature) iPutThisInformation(arg1 *gherkin.DataTable) (err error) {
@@ -84,10 +110,6 @@ func (a *apiFeature) sendRequestTo(method, endpoint string) (err error) {
 	return
 }
 
-func (a *apiFeature) theUserHasSetSomeInformation() error {
-	return godog.ErrPending
-}
-
 func (a *apiFeature) theResponseShouldInclude(arg1 *gherkin.DataTable) error {
 	return godog.ErrPending
 }
@@ -118,12 +140,12 @@ func (a *apiFeature) myResponseShouldInclude(arg1 *gherkin.DataTable) error {
 
 func FeatureContext(s *godog.Suite) {
 	api := &apiFeature{}
+	api.token = getToken()
 
 	s.BeforeScenario(api.resetResponse)
 	s.Step(`^I put this information$`, api.iPutThisInformation)
-	s.Step(`^send "([^"]*)" request to "([^"]*)"$`, api.sendRequestTo)
+	s.Step(`^I send "([^"]*)" request to "([^"]*)"$`, api.sendRequestTo)
 	s.Step(`^the response should be (\d+)$`, api.theResponseShouldBe)
-	s.Step(`^The user has set some information$`, api.theUserHasSetSomeInformation)
 	s.Step(`^the response should include$`, api.theResponseShouldInclude)
 	s.Step(`^the product test(\d+) does exist$`, api.theProductTestDoesExist)
 	s.Step(`^I request to create a product$`, api.iRequestToCreateAProduct)
