7fc0a433e3206fdd77f438505e8ccb362b05644e
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:50 +0100

    Added fail func and removed unused

--- api/main.go
+++ api/main.go
@@ -19,38 +19,18 @@ var conn *mongo.Database
 func main() {
 	//load the env file
 	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	failOnError(err, "Error getting env vars")
+
 	//Create a database connection
 	conn, err = configDB(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
+	failOnError(err, "Connecting to database failed")
+
 	router := mux.NewRouter()
-	//To allow other sources, enable cors
-	//router.Use(cors)
 
 	router.HandleFunc("/product/{barcode}", getProduct).Methods("GET")
 	router.HandleFunc("/product/{barcode}", updateProduct).Methods("POST")
 	router.HandleFunc("/vote/{barcode}", voteOnProduct).Methods("POST")
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router)
-}
-
-//To open the API to other sources (Browser ui) this will allow CORS
-func cors(h http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
-
-			if r.Method == "OPTIONS" {
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-		})
 }
 
 type CustomClaims struct {
@@ -75,4 +55,10 @@ func getUsername(r *http.Request) string {
 	fmt.Printf("(user) %s ", claims.Subject)
 	return claims.Subject
 
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
