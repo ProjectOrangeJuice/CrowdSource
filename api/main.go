fb3c711f28ea80c9a346a19978b186704e8a9170
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:50 +0100

    Initial search

--- api/main.go
+++ api/main.go
@@ -1,15 +1,58 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	router := mux.NewRouter()
 	//To allow other sources, enable cors
 	//router.Use(cors)
+
+	router.HandleFunc("/product/{barcode}", getProduct).Methods("GET")
+	http.ListenAndServe(":8000", router)
+}
+
+type product struct {
+	Name string `json:"product_name_it"`
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	barcode := params["barcode"]
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("test").Collection("Products")
+	filter := bson.M{"_id": barcode}
+	doc := collection.FindOne(context.TODO(), filter)
+	var fin product
+	doc.Decode(&fin)
+	fmt.Printf("%v", fin)
 }
 
 //To open the API to other sources (Browser ui) this will allow CORS
