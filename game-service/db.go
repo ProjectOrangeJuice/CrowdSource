aa7b8fe233dec1403b0908e24371f3c6fd0dcba9
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:50 +0100

    Setup game service

---
+++ game-service/db.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func configDB(ctx context.Context) (*mongo.Database, error) {
+	uri := fmt.Sprintf("mongodb://%s", os.Getenv("DBHOST"))
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, fmt.Errorf("couldn't connect to mongo: %v", err)
+	}
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("mongo client couldn't connect with background context: %v", err)
+	}
+	todoDB := client.Database("pro")
+	return todoDB, nil
+}
---
+++ game-service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var conn *mongo.Database
+
+func main() {
+	//load the env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	//Create a database connection
+	conn, err = configDB(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	router := mux.NewRouter()
+	//To allow other sources, enable cors
+	//router.Use(cors)
+
+	router.HandleFunc("/game/", getQuestion).Methods("GET")
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router)
+}
+
+//To open the API to other sources (Browser ui) this will allow CORS
+func cors(h http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+		})
+}
