96466c8db3de1256b04ded3d7bd4215ddd9513bd
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:50 +0100

    Get account information

---
+++ accountService/account.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type user struct {
+	Username      string `bson:"_id"`
+	Points        int
+	PointsHistory []histpoints
+}
+type histpoints struct {
+	Item      string
+	Version   int
+	Type      string
+	Points    int
+	Confirmed bool
+	Timestamp int64
+}
+
+func getAccount(username string) (user, error) {
+	var account user
+	collection := conn.Collection("user")
+	filter := bson.M{"_id": username}
+	doc := collection.FindOne(context.TODO(), filter)
+	err := doc.Decode(&account)
+	return account, err
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	account, err := getAccount(getUsername(r))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	output, _ := json.Marshal(account)
+	w.Write(output)
+
+}
