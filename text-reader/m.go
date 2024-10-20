6173a8e298179c9c09d2444f51e2f3b55c4b0569
ProjectOrangeJuice <git@harriso.co.uk>
2024-10-20 20:45:50 +0100

    Added sample

--- text-reader/m.go
+++ text-reader/m.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	client := gosseract.NewClient()
 	defer client.Close()
-	client.SetImage("./test.jpg")
+	client.SetImage("./test2.jpg")
 	text, _ := client.Text()
 	fmt.Println(text)
 	// Hello, World!
